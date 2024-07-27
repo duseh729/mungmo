878f0a5e3855269875f1dfa4fbef7104789fd2be
Doyeon <duseh729@naver.com>
2024-07-27 22:36:47 +0900

    fix: https -> http

--- src/apis/api/chat.js
+++ src/apis/api/chat.js
@@ -2,7 +2,7 @@ import { baseInstance } from "../utils/instance";
 
 export const postChat = async (message)=>{
   try{
-    const {data} = await baseInstance.post('/chat', {chatRoomSeq: 0, message: message})
+    const {data} = await baseInstance.post('/chat', { message: message})
     // console.log("api 내부 data", data);
     return data
   }catch(error){
