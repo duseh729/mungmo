2761378bcbe3756c826efbf2c37109730e0b0160
Doyeon <duseh729@naver.com>
2024-07-30 15:17:12 +0900

    feat: 견종별 소개말 수정

--- src/components/MainPage/LoginProfile.jsx
+++ src/components/MainPage/LoginProfile.jsx
@@ -3,7 +3,22 @@ import styles from "../../css/MainPage/Profile.module.scss";
 import { color } from "../../constant/style";
 
 const LoginProfile = ({ userName, dogType, profileContents }) => {
-  const dogs = ["골든 리트리버", "기본", "말티즈", "밥그릇", "비숑프리제", "시바 이누", "시츄", "웰시코기", "치와와", "포메라니안", "푸들", "프렌치 불독"];
+  
+  const dogs = [
+    { breed: "골든 리트리버", introduce: "충직하고 온순한 애교쟁이" },
+    { breed: "기본", introduce: "에너지 넘치는 사랑스런 귀염둥이" },
+    { breed: "말티즈", introduce: "애교 넘치는 천방지축 귀염둥이" },
+    { breed: "밥그릇", introduce: "?" },
+    { breed: "비숑프리제", introduce: "동글동글 심장폭격기 솜사탕" },
+    { breed: "시바 이누", introduce: "개성 넘치는 매력적인 엄살쟁이" },
+    { breed: "시츄", introduce: "활발하고 온순한 귀염둥이" },
+    { breed: "웰시코기", introduce: "호기심 많은 뒤태 매력 소유자" },
+    { breed: "치와와", introduce: "작은 체구의 용감한 파이터" },
+    { breed: "포메라니안", introduce: "작고 귀여운 에너지 왕" },
+    { breed: "푸들", introduce: "밝고 똑똑한 인기쟁이" },
+    { breed: "프렌치 불독", introduce: "영리하고 다정한 장난꾸러기" },
+  ];
+
   const contents = [
     ["나이", `${profileContents["birth"] - new Date().getFullYear() + 1}살`],
     ["몸무게", `${profileContents["weight"]}kg`],
@@ -27,7 +42,7 @@ const LoginProfile = ({ userName, dogType, profileContents }) => {
         }}
         className={styles.profileContainer}
       >
-        {dogs.find(dog => dog === dogType) ? <img src={`/img/dogProfile/${dogType}.png`} alt="" /> : <img src="/img/dogProfile/기본.png" alt="" />}
+        {dogs.find(dog => dog.breed === dogType) ? <img src={`/img/dogProfile/${dogType}.png`} alt="" /> : <img src="/img/dogProfile/기본.png" alt="" />}
 
         <div style={{ textAlign: "center" }}>
           <span style={{ color: color.primaryColor, backgroundColor: "white" }} className={`semi-bold-text text14 letter-spacing2 ${styles.profileState}`}>
@@ -39,7 +54,7 @@ const LoginProfile = ({ userName, dogType, profileContents }) => {
           </h1>
 
           <p style={{ color: "white" }} className="text14 letter-spacing2">
-            {profileContents.info}
+            {dogs.find(dog => dog.breed === dogType)?.introduce || "에너지 넘치는 사랑스런 귀염둥이"} 
           </p>
         </div>
 
